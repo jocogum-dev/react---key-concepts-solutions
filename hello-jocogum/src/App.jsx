9c8c3f86d20d66e6ad6f9bb756c3f664909f26df
jocogum-dev <jocogum10@gmail.com>
2025-06-15 13:05:37 +0800

    add first exercise

---
+++ hello-jocogum/src/App.jsx
@@ -0,0 +1,21 @@
+import img from "./assets/image.png"
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <img src={img} alt="" />
+      <h1>Hello World, this is jocogum</h1>
+      <p>I am a frontend developer</p>
+    </header>
+    <main>
+      <p>Right now, I am trying to complete the roadmap.sh frontend projects.</p>
+      <p>I post my unedited, uncut coding sessions on my youtube channel.</p>
+      <p>If you are interested, like and subscribe!</p>
+    </main>
+    </>
+  )
+}
+
+export default App
